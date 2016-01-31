52d6d567d949e4a731880cec5653c91e22ae5ed4
AlexPlex <atellmer@gmail.com>
2016-01-31 19:50:28 +0500

    Add toolbar

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,3 +1,10 @@
 'use strict';
 
 var app = angular.module('app', ['ngMaterial']);
+
+app.config(function($mdIconProvider) {
+  $mdIconProvider.fontSet('fa', 'fontawesome');
+});
+
+app.controller('AppCtrl', function($scope) {
+});
