91ac09202e3e862c9e50e7dad8a321e57d226f6e
AlexPlex <atellmer@gmail.com>
2016-01-31 17:18:59 +0500

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+		stylus = require('gulp-stylus'),
+		concatCss = require('gulp-concat-css'),
+		minifyCSS = require('gulp-minify-css'),
+		rename = require('gulp-rename'),
+		livereload = require('gulp-livereload'),
+		connect = require('gulp-connect');
+
+//connect
+gulp.task('connect', function() {
+	connect.server({
+		root: 'app',
+		 port: 3000,
+		livereload: true
+		});
+	});
+
+//html
+gulp.task('html', function() {
+	gulp.src('app/*.html')
+	.pipe(connect.reload());
+});
+
+
+//stylus
+gulp.task('styl', function () {
+   gulp.src('app/stylus/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('app/css/'))
+    .pipe(connect.reload());
+});
+
+
+//css
+gulp.task('css', function() {
+	return gulp.src('app/css/*.css')
+	.pipe(concatCss("bundle.css"))
+	.pipe(minifyCSS())
+	.pipe(rename('css/production/bundle.min.css'))
+	.pipe(gulp.dest('app/'))
+	.pipe(connect.reload());
+	});
+
+//watch
+gulp.task('watch', function() {
+	gulp.watch('app/stylus/*.styl', ['styl']);
+	gulp.watch('app/css/*.css', ['css']);
+	gulp.watch('app/*.html', ['html']);
+	});
+
+gulp.task('default', ['connect', 'html', 'styl', 'css', 'watch']);
