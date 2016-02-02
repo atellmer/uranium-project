09d0a2431c5b82c0ac2e38792ad9f4acbbb95fd3
AlexPlex <atellmer@gmail.com>
2016-02-02 22:45:24 +0500

    add lazzy load and optimaze number of wathcers from ng-repeat directive

--- client/app/controllers/posts.controller.js
+++ client/app/controllers/posts.controller.js
@@ -2,16 +2,42 @@
 	'use strict';
 
 	angular
-    .module('app')
-    .controller('PostsCtrl', PostsCtrl);
+  .module('app')
+  .controller('PostsCtrl', PostsCtrl);
 
-  PostsCtrl.$inject = ['$scope', '$http'];
+  PostsCtrl.$inject = ['$scope', '$http', '$timeout', '$window'];
 
-  function PostsCtrl($scope, $http) {
+  function PostsCtrl($scope, $http, $timeout, $window) {
+    var cash,
+        last,
+        delta = 3,
+        initial = 6,
+        path = '../app/models/posts.model.json';
 
-  	$http.get('../app/models/posts.model.json')
-  			 .success(function(response) {
-						$scope.cards = response;
-					});
+    $http.get(path).success(function(response) {
+      cash = response;
+      $scope.cards = response.slice(0, initial);
+      last = delta;
+    });
+
+    angular.element($window).bind('scroll', scrollHandler);
+
+    function loadMoreItems() {
+      delta += last;
+      $scope.cards = cash.slice(last, delta);
+    }
+
+    function scrollHandler() {
+      var position,
+          end;
+
+      position = angular.element(this).scrollTop() + angular.element(this).height(),
+      end = angular.element('#end-content').offset().top;
+
+      if(position >= end) {
+        $timeout(loadMoreItems, 0);
+      }
+    }
+
   }
 })();
