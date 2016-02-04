46f82231f9ab4f0d887feb29736bdc99d1564322
AlexPlex <atellmer@gmail.com>
2016-02-04 16:57:26 +0500

    add float action button

---
+++ client/app/controllers/fab.controller.js
@@ -0,0 +1,26 @@
+;(function() {
+	'use strict';
+
+	angular
+  .module('app')
+  .controller('FABCtrl', FABCtrl);
+
+  FABCtrl.$inject = ['$scope'];
+
+  function FABCtrl($scope) {
+    this.isOpen = false;
+    this.selectedMode = 'md-fling';
+    this.selectedDirection = 'up';
+    this.hover = false;
+
+    $scope.moveUp = function() {
+      return moveUp();
+    }
+
+    function moveUp() {
+      angular.element("body,html").animate({scrollTop:0}, 800);
+    }
+  }
+
+
+ })();
