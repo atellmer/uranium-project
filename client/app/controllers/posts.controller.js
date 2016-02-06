93413d5243434d5c4a77ecb522f9a2674fc380fd
AlexPlex <atellmer@gmail.com>
2016-02-06 19:23:30 +0500

    fixed bag scroll in Edge

--- client/app/controllers/posts.controller.js
+++ client/app/controllers/posts.controller.js
@@ -1,50 +1,53 @@
-;(function() {
+;
+(function () {
 	'use strict';
-
 	angular
-  .module('app')
-  .controller('PostsCtrl', PostsCtrl);
+		.module('app')
+		.controller('PostsCtrl', PostsCtrl);
 
-  PostsCtrl.$inject = ['$scope', '$http', '$timeout', '$window'];
+	PostsCtrl.$inject = ['$scope', '$http', '$window'];
 
-  function PostsCtrl($scope, $http, $timeout, $window) {
-    var cash,
-        last,
-        delta,
-        initial = 6,
-        path = '../app/models/posts.model.json';
+	function PostsCtrl($scope, $http, $window) {
+		var cash,
+			last,
+			delta,
+			initial = 6,
+			path = '../app/models/posts.model.json';
 
-    $http.get(path).success(function(response) {
-      last = 0;
-      delta = 3;
-      cash = {};
-      $scope.cards = {};
+		$http.get(path).success(function (response) {
+			last = 0;
+			delta = 3;
+			cash = {};
+			$scope.cards = {};
 
-      cash = response;
+			cash = response;
 
-      $scope.cards = cash.slice(0, initial);
-      last = delta;
-    });
+			$scope.cards = cash.slice(0, initial);
+			last = delta;
+		});
 
-    angular.element($window).bind('scroll', scrollHandler);
+		angular.element($window).bind('scroll', scrollHandler);
 
-    function loadMoreItems() {
-      delta += last;
-      if(cash) {
-        $scope.cards = cash.slice(last, delta);
-      }
-    }
+		function loadMoreItems() {
+			if (cash && (delta + last < cash.length)) {
 
-    function scrollHandler() {
-      var position,
-          end;
+				last = $scope.cards.length;
 
-      position = angular.element(this).scrollTop() + angular.element(this).height(),
-      end = angular.element('#end-content').offset().top;
+				var cards = $scope.cards.concat(cash.slice(last, last + delta));
+				$scope.cards = cards;
+			}
+		}
 
-      if(position >= end) {
-        $scope.$apply(loadMoreItems);
-      }
-    }
-  }
+		function scrollHandler() {
+			var position,
+				end;
+
+			position = angular.element(this).scrollTop() + angular.element(this).height();
+			end = angular.element('#end-content').offset().top;
+
+			if (position >= end) {
+				$scope.$apply(loadMoreItems);
+			}
+		}
+	}
 })();
