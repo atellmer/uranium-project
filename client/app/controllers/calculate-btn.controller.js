9447fa444310221cfe8677340482ea844957df64
AlexPlex <atellmer@gmail.com>
2016-02-15 16:43:03 +0500

    add ortfolio-block, add scrollTo

---
+++ client/app/controllers/calculate-btn.controller.js
@@ -0,0 +1,26 @@
+;
+(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('CalculateBtnCtrl', CalculateBtnCtrl);
+
+	CalculateBtnCtrl.$inject = ['$scope'];
+
+	function CalculateBtnCtrl($scope) {
+
+		var element = angular.element('#scroll-here');
+
+		$scope.go = function () {
+			return go();
+		}
+
+		function go() {
+			angular.element("body,html").animate({
+				scrollTop: (element.offset().top - 50)
+			}, 500);
+		}
+	}
+
+})();
