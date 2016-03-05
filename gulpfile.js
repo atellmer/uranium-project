c8754fdc712c986acaaf59dba9e5fc048e987a82
AlexPlex <atellmer@gmail.com>
2016-03-05 14:39:25 +0500

    refactoring structure code

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,73 @@
 var gulp = require('gulp'),
-		stylus = require('gulp-stylus'),
-		concatCss = require('gulp-concat-css'),
-		minifyCSS = require('gulp-minify-css'),
-		rename = require('gulp-rename'),
-		livereload = require('gulp-livereload'),
-		connect = require('gulp-connect');
-		concat = require('gulp-concat');
-		uglify = require('gulp-uglify');
+	stylus = require('gulp-stylus'),
+	nib = require('nib'),
+	rename = require('gulp-rename'),
+	connect = require('gulp-connect'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
+
+
+var path = {
+	root: 'client/',
+	stylus: function () {
+		return this.root + 'stylesheets/stylus/'
+	},
+	css: function () {
+		return this.root + 'stylesheets/css/'
+	}
+}
 
 //connect
-gulp.task('connect', function() {
+gulp.task('connect', function () {
 	connect.server({
 		root: 'client',
-		 port: 3000,
+		port: 3000,
 		livereload: true
-		});
 	});
+});
 
 //js
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
 	return gulp.src([
-		'client/app/**/*.module.js', 
-		'client/app/**/*.config.js',
-		'client/app/**/*.controller.js'
+		path.root + 'app/**/*.module.js',
+		path.root + 'app/**/*.config.js',
+		path.root + 'app/**/*.controller.js'
 	])
-	.pipe(concat('bundle.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('client/app/dist/'))
-	.pipe(connect.reload());
+		.pipe(concat('bundle.js'))
+		.pipe(uglify())
+		.pipe(rename('bundle.min.js'))
+		.pipe(gulp.dest('client/app/dist/'))
+		.pipe(connect.reload());
 });
-
-//html
-gulp.task('html', function() {
-	return gulp.src('client/*.html')
-	.pipe(connect.reload());
-});
-
 
 //stylus
 gulp.task('styl', function () {
-   return gulp.src('client/stylus/*.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('client/css/'))
-    .pipe(connect.reload());
+	return gulp.src(path.stylus() + '*.styl')
+		.pipe(concat('bundle.styl'))
+		.pipe(stylus({
+			use: [nib()],
+			compress: true
+		}))
+		.pipe(rename('bundle.min.css'))
+		.pipe(gulp.dest(path.css()))
+		.pipe(connect.reload());
 });
 
-
-//css
-gulp.task('css', function() {
-	return gulp.src('client/css/*.css')
-	.pipe(concatCss("bundle.css"))
-	.pipe(minifyCSS())
-	.pipe(rename('css/dist/bundle.min.css'))
-	.pipe(gulp.dest('client/'))
-	.pipe(connect.reload());
-	});
+//html
+gulp.task('html', function () {
+	return gulp.src([
+				path.root + 'index.html',
+				path.root + 'app/views/*.html'
+			])
+		.pipe(connect.reload());
+});
 
 //watch
-gulp.task('watch', function() {
-	gulp.watch('client/stylus/*.styl', ['styl']);
-	gulp.watch('client/css/*.css', ['css']);
-	gulp.watch('client/*.html', ['html']);
-	gulp.watch('client/app/**/*.*', ['scripts']);
-	});
+gulp.task('watch', function () {
+	gulp.watch(path.root + 'app/**/*.*', ['scripts']);
+	gulp.watch(path.stylus() + '*.styl', ['styl']);
+	gulp.watch([path.root + 'index.html', path.root + 'app/views/*.html'], ['html']);
 
-gulp.task('default', ['connect', 'scripts', 'html', 'styl', 'css', 'watch']);
+});
+
+gulp.task('default', ['connect', 'scripts', 'styl', 'html', 'watch']);
